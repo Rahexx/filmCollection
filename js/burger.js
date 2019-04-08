62ea4e9eb233085b482e7d52951935cb3498472e
Andrzej Łuszcz <endriu1297@gmail.com>
2019-04-08 16:21:03 +0200

    Adding comments to the burger file

--- js/burger.js
+++ js/burger.js
@@ -1,15 +1,16 @@
-class Burger{
-    constructor(){
+// The class that supports the event on the burger
+class Burger {
+    constructor() {
         this.nav = document.querySelector('nav');
         this.bars = document.querySelector('.fa-bars');
         this.times = document.querySelector('.fa-times');
         document.querySelector('.burger').addEventListener('click', this.showBurger.bind(this));
-    } 
+    }
 
-    showBurger(){
+    // Function that changes the appearance of the burger and the appearance or disappearance of navigation
+    showBurger() {
         this.nav.classList.toggle('on');
         this.bars.classList.toggle('on');
         this.times.classList.toggle('on');
     }
 }
-
